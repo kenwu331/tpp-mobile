68675ca20ae10f7ba6b62aa94dbda9c03dee0729
Ken-Ng <124976380@qq.com>
2018-02-26 22:39:09 +0800

    fixed the miss file!

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
     <tab-main></tab-main>
-    <!-- <router-view></router-view> -->
+    <router-view></router-view>
+    <city v-if="$store.state.cityOpen"></city>
   </div>
 </template>
 
 <script>
 import tabMain from '@/components/tab/tab-main'
+import city from '@/components/display-page/city'
 export default {
   name: 'app',
   components:{
-    tabMain
+    tabMain,
+    city
   }
 }
 </script>
 
 <style>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+#app {
+  font-family: Helvetica Neue,Helvetica,Arial,Hiragino Sans GB,Microsoft Yahei,STXihei,SimHei,WenQuanYi Micro Hei,Droid Sans,sans-serif;
+  /* -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-} */
+  margin-top: 60px; */
+}
 blockquote, body, button, dd, dl, dt, fieldset, form, h1, h2, h3, h4, h5, h6, hr, input, legend, li, ol, p, pre, td, textarea, th, ul{
     margin: 0;
     padding: 0;
---
+++ src/components/nav/nav-main.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="nav-main">
+        <div class="nav-city" @click="cityOpen">
+            <div class="pic"></div>
+            <div>{{$store.state.city}}</div>
+            <div class="v"></div>
+        </div>
+        <div class="nav-item">
+            <div class="item" :class="{'current':currentItem==index}" 
+            :key="index" v-for="(val,index) in item" 
+            @click="choiceNav(index)">{{val}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return{
+            item:['正在热映','即将上映'],
+            currentItem:0,
+            itemRouter:['/main/','/main/pre']
+        }
+    },
+    methods:{
+        choiceNav(i){
+            this.currentItem=i
+            this.$router.push(this.itemRouter[i])
+        },
+        cityOpen(){
+            this.$store.commit('cityOpen')
+        }
+    }
+}
+</script>
+
+<style>
+    .nav-main{
+        width:100%;
+        position:fixed;
+        top:0;
+        height:11vmin;
+        overflow: hidden;
+        color:#999;
+        background:#fff;
+    }
+    .nav-city{
+        width:42%;
+        height:11vmin;
+        line-height: 11vmin;
+        box-sizing: border-box;
+        padding-left:5vmin;
+        float: left;
+    }
+    .nav-city div{
+        float: left;
+        position:relative;
+        top:50%;
+        transform: translateY(-50%);
+        margin-right:2vmin;
+    }
+    .nav-city .pic{
+        width:25px;
+        height:25px;
+        background:url(./../../../static/img/logo.png);
+    }
+    .nav-city .v{
+        width:2vmin;
+        height:2vmin;
+        border-bottom:0.2vmin #999 solid;
+        border-left:0.2vmin #999 solid;
+        transform:rotate(-45deg);
+        top:30%;
+    }
+    .nav-item{
+        float: left;
+        width:40vmin;
+        height:11vmin;
+        display: flex;
+        text-align: center;
+        line-height: 11vmin;
+        font-size:3.5vmin;
+    }
+    .nav-item .item{
+        flex:1;       
+    }
+    .nav-item .current{
+        border-bottom:0.8vmin #ff4d64 solid;
+        color:#ff4d64;
+    }
+</style>
